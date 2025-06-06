599703644fd8a8ec651c9ceedc63a59fe888a072
TheErgster <Erg95@students.needham.k12.ma.us>
2025-06-06 15:44:50 -0400

    Initial commit of FTC Summer Project

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/LO2/SummerAuto.java
@@ -0,0 +1,228 @@
+package org.firstinspires.ftc.robotcontroller.LO2;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class SummerAuto extends OpMode {
+    private DcMotorEx frontLeft;
+    private DcMotorEx frontRight;
+    private DcMotorEx backRight;
+    private DcMotorEx backLeft;
+
+    // CONSTANTS: change these to  robot specs
+
+    // Encoder ticks per motor revolution (hardware-specific)
+    final double TICKS_PER_REV = 537.7;
+
+    // Wheel diameter in centimeters
+    final double WHEEL_DIAMETER = 9.6;
+
+    // If each corner of the robot was a wheel what would the specs be
+    final double ROBOT_WIDTH = 40;
+    final double ROBOT_LENGTH = 35;
+
+    // Calculate cm traveled per encoder tick:
+    // Equation: cm_per_tick = (π × wheel_diameter) / TICKS_PER_REV
+    final double CM_PER_TICK = (Math.PI * WHEEL_DIAMETER) / TICKS_PER_REV;
+
+    // Calculate the turn radius (distance from robot center to wheel):
+    // Equation: turn_radius = sqrt(width² + length²) / 2
+    final double TURN_RADIUS = Math.sqrt(Math.pow(ROBOT_WIDTH, 2) + Math.pow(ROBOT_LENGTH, 2)) / 2;
+
+    // Calculate cm the wheels must travel to turn robot 1 degree:
+    // Equation: cm_per_degree = (2 × π × turn_radius) / 360
+    final double CM_PER_DEGREE = (2 * Math.PI * TURN_RADIUS) / 360;
+
+    // Max velocity of the motor in ticks per second (adjust for  motors)
+    final double MAX_TICKS_PER_SECOND = 2300;
+
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "fl");
+        frontRight = hardwareMap.get(DcMotorEx.class, "fr");
+        backLeft = hardwareMap.get(DcMotorEx.class, "bl");
+        backRight = hardwareMap.get(DcMotorEx.class, "br");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addLine("Initiated");
+        telemetry.update();
+    }
+
+    // takes y (forward), rx (rotation), x (strafe) powers between -1 and 1
+    public void driveOmni(double y, double rx, double x) {
+        double maxValue = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        double flPower = (y + x + rx) / maxValue;
+        double blPower = (y - x + rx) / maxValue;
+        double frPower = (y - x - rx) / maxValue;
+        double brPower = (y + x - rx) / maxValue;
+
+        frontLeft.setVelocity(flPower * MAX_TICKS_PER_SECOND);
+        backLeft.setVelocity(blPower * MAX_TICKS_PER_SECOND);
+        frontRight.setVelocity(frPower * MAX_TICKS_PER_SECOND);
+        backRight.setVelocity(brPower * MAX_TICKS_PER_SECOND);
+    }
+
+    // Move  a given distance (cm) + is forward - is back
+    public void move(double distanceCm) {
+        // Calculate encoder ticks needed for desired distance
+        int targetTicks = (int)(distanceCm / CM_PER_TICK);
+
+        // Reset encoders before movement
+        resetEncoders();
+
+        // Set motors to run to target position
+        frontLeft.setTargetPosition(targetTicks);
+        backLeft.setTargetPosition(targetTicks);
+        frontRight.setTargetPosition(targetTicks);
+        backRight.setTargetPosition(targetTicks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Set motor power forward
+        frontLeft.setPower(0.5);
+        backLeft.setPower(0.5);
+        frontRight.setPower(0.5);
+        backRight.setPower(0.5);
+
+        // Wait until motors reach the target position
+        while (frontLeft.isBusy() && backLeft.isBusy() && frontRight.isBusy() && backRight.isBusy()) {
+            //view movement
+            telemetry.addData("Moving forward", "Ticks %d/%d", frontLeft.getCurrentPosition(), targetTicks);
+            telemetry.update();
+        }
+
+        stopMotors();
+    }
+
+    // Strafe a given distance (cm) (positive is right - is left)
+    public void strafe(double distanceCm) {
+        int targetTicks = (int)(distanceCm / CM_PER_TICK);
+
+        resetEncoders();
+
+        // Set targets for strafing right
+        frontLeft.setTargetPosition(targetTicks);
+        backLeft.setTargetPosition(-targetTicks);
+        frontRight.setTargetPosition(-targetTicks);
+        backRight.setTargetPosition(targetTicks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Power to strafe right
+        frontLeft.setPower(0.5);
+        backLeft.setPower(0.5);
+        frontRight.setPower(0.5);
+        backRight.setPower(0.5);
+
+        while (frontLeft.isBusy() && backLeft.isBusy() && frontRight.isBusy() && backRight.isBusy()) {
+            telemetry.addData("Strafing", "Ticks %d/%d", frontLeft.getCurrentPosition(), targetTicks);
+            telemetry.update();
+        }
+
+        stopMotors();
+    }
+
+    // Rotate robot by degrees (+ for clockwise, - for counterclockwise)
+    public void rotate(double degrees) {
+        // Calculate distance each wheel must travel to rotate desired degrees
+        double distanceCm = degrees * CM_PER_DEGREE;
+
+        // Calculate ticks for this rotation distance
+        int targetTicks = (int)(distanceCm / CM_PER_TICK);
+
+        resetEncoders();
+
+        // Set motor targets for rotation (opposite directions on left/right wheels)
+        frontLeft.setTargetPosition(targetTicks);
+        backLeft.setTargetPosition(targetTicks);
+        frontRight.setTargetPosition(-targetTicks);
+        backRight.setTargetPosition(-targetTicks);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Set motor power for rotation
+        frontLeft.setPower(0.5);
+        backLeft.setPower(0.5);
+        frontRight.setPower(0.5);
+        backRight.setPower(0.5);
+
+        while (frontLeft.isBusy() && backLeft.isBusy() && frontRight.isBusy() && backRight.isBusy()) {
+            telemetry.addData("Rotating", "Ticks %d/%d", frontLeft.getCurrentPosition(), targetTicks);
+            telemetry.update();
+        }
+
+        stopMotors();
+    }
+
+    // Utility method: stop all motors
+    public void stopMotors() {
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+    }
+
+    // Utility method: reset encoders to zero
+    public void resetEncoders() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    @Override
+    public void start() {
+        telemetry.addLine("Auto start");
+        telemetry.update();
+
+        // test autonomous routine:
+
+        move(50);
+        strafe(30);
+        rotate(90);
+        move(20);
+        rotate(-45);
+
+        telemetry.addLine("Auto complete");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        // Autonomous runs everything in start()
+    }
+
+    @Override
+    public void stop() {
+        stopMotors();
+        telemetry.addLine("Stopped");
+        telemetry.update();
+    }
+}
---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/LO2/SummerOp.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.robotcontroller.LO2;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class SummerOp extends OpMode {
+    String gamepad1Buttons = "";
+    //Create gamepad 1 telemetry
+    String gamepad2Buttons = "";
+    //Create gamepad 2 telemetry
+    private DcMotorEx frontLeft;
+    //create motor variable with Ex typing
+    private DcMotorEx frontRight;
+    private DcMotorEx backRight;
+    private DcMotorEx backLeft;
+
+    @Override
+    public void init() {
+        //defines motors for configuration (put "fr" or something else in the config)
+        frontLeft = hardwareMap.get(DcMotorEx.class, "fl");
+        frontRight = hardwareMap.get(DcMotorEx.class, "fr");
+        backLeft = hardwareMap.get(DcMotorEx.class, "bl");
+        backRight = hardwareMap.get(DcMotorEx.class, "br");
+        //Direction defined
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        //Encoder initiation
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addLine("Initiated");
+        telemetry.update();
+    }
+
+    public void driveOmni(double y, double rx, double x) {
+        //Math stuff stolen from the internet
+        double maxValue = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        double flPower = (y + x + rx) / maxValue;
+        double blPower = (y - x + rx) / maxValue;
+        double frPower = (y - x - rx) / maxValue;
+        double brPower = (y + x - rx) / maxValue;
+
+        final double MAX_TICKS_PER_SECOND = 2300; // change based on motor type I know we use a goBilda something
+
+        frontLeft.setVelocity(flPower * MAX_TICKS_PER_SECOND);
+        backLeft.setVelocity(blPower * MAX_TICKS_PER_SECOND);
+        frontRight.setVelocity(frPower * MAX_TICKS_PER_SECOND);
+        backRight.setVelocity(brPower * MAX_TICKS_PER_SECOND);
+    }
+
+    //When play is pressed
+    public void start() {
+            telemetry.addLine("Go go go!!!!!");
+            telemetry.update();
+        }
+    //When stop is pressed
+    public void stop(){
+       telemetry.addLine("Stopped");
+       telemetry.update();
+    }
+
+    public void loop() {
+        driveOmni(-1*gamepad1.left_stick_y, 1*gamepad1.right_stick_x, 1*gamepad1.left_stick_x);
+
+        //Telemetry for all buttons
+        if (gamepad1.a) gamepad1Buttons  = "1A ";
+        else if (gamepad1.b) gamepad1Buttons  = "1B ";
+        else if (gamepad1.x) gamepad1Buttons  = "1X ";
+        else if (gamepad1.y) gamepad1Buttons  = "1Y ";
+        else if (gamepad1.left_bumper) gamepad1Buttons  = "1LB ";
+        else if (gamepad1.right_bumper) gamepad1Buttons  = "1RB ";
+        else if (gamepad1.dpad_down) gamepad1Buttons  = "1Dpad Down ";
+        else if (gamepad1.dpad_up) gamepad1Buttons  = "1Dpad Up ";
+        else if (gamepad1.dpad_left) gamepad1Buttons  = "1Dpad Left ";
+        else if (gamepad1.dpad_right) gamepad1Buttons  = "1Dpad Right ";
+        else if (gamepad1.left_stick_x > 0) gamepad1Buttons  = "1Left Stick ";
+        else if (gamepad1.left_stick_y > 0) gamepad1Buttons  = "1Left Stick ";
+        else if (gamepad1.right_stick_x > 0) gamepad1Buttons  = "1Right Stick ";
+        else if (gamepad1.right_stick_y > 0) gamepad1Buttons  = "1Right Stick ";
+        else if (gamepad1.start) gamepad1Buttons  = "1Start ";
+        else if (gamepad1.back) gamepad1Buttons  = "1Back ";
+        else if (gamepad1.left_trigger > 0) gamepad1Buttons  = "1Left Trigger ";
+        else if (gamepad1.right_trigger > 0) gamepad1Buttons  = "1Right Trigger ";
+        else gamepad1Buttons="";
+
+
+        telemetry.addData("Gamepad 1", gamepad1Buttons);
+
+
+        if (gamepad2.a) gamepad2Buttons  = "2A ";
+        else if (gamepad2.b) gamepad2Buttons  = "2B ";
+        else if (gamepad2.x) gamepad2Buttons  = "2X ";
+        else if (gamepad2.y) gamepad2Buttons  = "2Y ";
+        else if (gamepad2.left_bumper) gamepad2Buttons  = "2LB ";
+        else if (gamepad2.right_bumper) gamepad2Buttons  = "2RB ";
+        else if (gamepad2.dpad_down) gamepad2Buttons  = "2Dpad Down ";
+        else if (gamepad2.dpad_up) gamepad2Buttons  = "2Dpad Up ";
+        else if (gamepad2.dpad_left) gamepad2Buttons  = "2Dpad Left ";
+        else if (gamepad2.dpad_right) gamepad2Buttons  = "2Dpad Right ";
+        else if (gamepad2.left_stick_x > 0) gamepad2Buttons  = "2Left Stick ";
+        else if (gamepad2.left_stick_y > 0) gamepad2Buttons  = "2Left Stick ";
+        else if (gamepad2.right_stick_x > 0) gamepad2Buttons  = "2Right Stick ";
+        else if (gamepad2.right_stick_y > 0) gamepad2Buttons  = "2Right Stick ";
+        else if (gamepad2.start) gamepad2Buttons  = "2Start ";
+        else if (gamepad2.back) gamepad2Buttons  = "2Back ";
+        else if (gamepad2.left_trigger > 0) gamepad2Buttons  = "2Left Trigger ";
+        else if (gamepad2.right_trigger > 0) gamepad2Buttons  = "2Right Trigger ";
+        else gamepad1Buttons="";
+
+        telemetry.addData("Gamepad 2", gamepad2Buttons);
+
+        telemetry.update();
+    }
+}
